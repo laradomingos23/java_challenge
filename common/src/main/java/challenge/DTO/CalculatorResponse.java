4127cd05b4a591d6b4ad59942b96a91a35bdeb23
Lara Domingos <laradomingos2001@hotmail.com>
2025-04-30 17:11:26 +0100

    add method equals and hashcode for testing

--- common/src/main/java/challenge/DTO/CalculatorResponse.java
+++ common/src/main/java/challenge/DTO/CalculatorResponse.java
@@ -1,14 +1,16 @@
 package challenge.DTO;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class CalculatorResponse {
     private String requestId;
     private BigDecimal result;
 
-    public CalculatorResponse(){}    
-    
-    public CalculatorResponse(String requestId, BigDecimal result){
+    public CalculatorResponse() {
+    }
+
+    public CalculatorResponse(String requestId, BigDecimal result) {
         this.requestId = requestId;
         this.result = result;
     }
@@ -16,14 +18,32 @@ public class CalculatorResponse {
     public String getRequestId() {
         return this.requestId;
     }
+
     public void setRequestId(String requestId) {
         this.requestId = requestId;
     }
+
     public BigDecimal getResult() {
         return this.result;
     }
+
     public void setResult(BigDecimal result) {
         this.result = result;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        CalculatorResponse that = (CalculatorResponse) o;
+        return Objects.equals(requestId, that.requestId) &&
+                Objects.equals(result, that.result);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(requestId, result);
+    }
 }
